426df46217e567b7644a67ae966c97ab28856b4f
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-31 16:16:43 +0100

    Base for testing DB implementation.

---
+++ Jenkinsfile
@@ -0,0 +1,29 @@
+pipeline{
+    agent any
+    environment{
+        DATABASE_URI = credentials('DATABASE_URI')
+        MYSQL_ROOT_PASSWORD = credentials('DB_PASSWORD')
+    }
+    stages{
+        stage("Make Scripts Executable"){
+            steps{
+                sh 'chmod +x ./scripts/*'
+            }
+        }
+        stage("Install Ansible and Run Playbook."){
+            steps{
+                sh './scripts/ansible.sh'
+            }
+        }
+        stage("Docker deployment via stack."){
+            steps{
+                sh './scripts/docker.sh'
+            }
+        }
+        stage("Deploy via swarm") {
+            steps {
+                sh './scripts/deploy.sh'
+            }        
+        }
+    }
+}
---
+++ ui/application/routes.py
@@ -0,0 +1,21 @@
+from application import app, db
+from flask import render_template, jsonify
+import requests
+import json
+from application.models import prizesgiven
+
+@app.route('/', methods=['GET'])
+def home():
+	return render_template('home.html', title='Home')
+
+@app.route('/get/prize', methods=['GET','POST'])
+def prize():
+	letter = requests.get('http://lgen:5001/prize/lgen')
+	number = requests.get('http://ngen:5002/prize/ngen')
+	prize = requests.post('http://pgen:5003/prize/pgen', json={'Letter':letter.text, 'Number':number.text})
+
+	db_data_set = prizesgiven(pool=letter.text, value=number.text, prize=prize.text)
+	db.session.add(db_data_set)
+	db.session.commit()
+
+	return render_template('prize.html', title='Prize', prize=prize.text)
